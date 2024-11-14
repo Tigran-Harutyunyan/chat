e52d2a8a015cf86bf15fd8addd0b320bacc35128
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-14 15:56:58 +0400

    created layout for auth pages

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useAuthGuard } from "../composables/useAuthGuard";
+import { useAuth } from "vue-clerk";
+
+const { userId, isLoaded } = useAuth();
+const isAuthed = isLoaded.value && !!userId.value;
+useAuthGuard(isAuthed);
+</script>
+
+<template>
+  <div class="h-full flex items-center justify-center bg-[#5C3B58]">
+    <div class="md:h-auto md:w-[420px]">
+      <slot />
+    </div>
+  </div>
+</template>
