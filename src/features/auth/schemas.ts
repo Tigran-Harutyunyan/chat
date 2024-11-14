3e11001cee0b75d5cb460f2baf74dd99260ea8fd
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-14 15:59:07 +0400

    created zod schemas  for auth requests

---
+++ src/features/auth/schemas.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { toTypedSchema } from "@vee-validate/zod";
+
+export const verificationSchema = toTypedSchema(z.object({
+  code: z.string().min(6, "Minimum of 6 characters required"),
+}));
+
+export const loginSchema = toTypedSchema(z.object({
+  emailAddress: z.string().email(),
+  password: z.string().min(8, "Required"),
+}));
+
+export const registerSchema = toTypedSchema(z.object({
+  firstName: z.string().min(2, "Minimum of 2 characters required"),
+  lastName: z.string().min(2, "Minimum of 2 characters required"),
+  emailAddress: z.string().email(),
+  password: z.string().min(8, "Minimum of 8 characters required"),
+  confirmPassword: z.string().min(8, "Minimum of 8 characters required"),
+}).superRefine(({ confirmPassword, password }, ctx) => {
+  if (confirmPassword !== password) {
+    ctx.addIssue({
+      code: "custom",
+      message: "The passwords did not match",
+      path: ['confirmPassword']
+    });
+  }
+}));
+
