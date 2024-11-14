b08c25b60cff9b92ce3ebae20375982c2a5548de
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-14 15:58:19 +0400

    initial App.vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Loader2 } from "lucide-vue-next";
+import { ClerkLoaded, ClerkLoading } from "vue-clerk";
+</script>
+
+<template>
+  <ClerkLoading>
+    <Loader2 class="h-8 w-8 animate-spin text-zinc-500" />
+  </ClerkLoading>
+  <ClerkLoaded>
+    <RouterView />
+  </ClerkLoaded>
+</template>
