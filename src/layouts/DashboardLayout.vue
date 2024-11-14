594419686d563efeaae3d0600127a4b6293852fd
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-14 16:01:39 +0400

    created initial layout for dashboard

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useAuth } from "vue-clerk";
+import { useRouteGuard } from "../composables/useRouteGuard";
+
+const { userId, isLoaded } = useAuth();
+const isAuthed = isLoaded.value && !!userId.value;
+useRouteGuard(isAuthed);
+</script>
+
+<template>
+  <slot></slot>
+</template>
