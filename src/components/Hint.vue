5efb269df8721c7c0a5b9b90f25327210816c9d8
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-16 12:36:04 +0400

    created reusable Hint component based on tooltip

---
+++ src/components/Hint.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface HintProps {
+  label: string;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+defineProps<HintProps>();
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip :delayDuration="50">
+      <TooltipTrigger as-child>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent
+        :side="side"
+        :align="align"
+        class="bg-black text-white border border-white/5"
+      >
+        <p class="font-medium text-xs">
+          {{ label }}
+        </p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
