af430aa2c0ddf1e0e52d599295a98a4c3e2cb6aa
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-16 14:31:23 +0400

    improved initial loading

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-import { Loader2 } from "lucide-vue-next";
+import { Loader } from "lucide-vue-next";
 import { ClerkLoaded, ClerkLoading } from "vue-clerk";
+import { Toaster } from "@/components/ui/sonner";
 </script>
 
 <template>
   <ClerkLoading>
-    <Loader2 class="h-8 w-8 animate-spin text-zinc-500" />
+    <div class="h-full flex items-center justify-center">
+      <Loader class="size-6 animate-spin text-muted-foreground" />
+    </div>
   </ClerkLoading>
   <ClerkLoaded>
+    <Toaster />
     <RouterView />
   </ClerkLoaded>
 </template>
