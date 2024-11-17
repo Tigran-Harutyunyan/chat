7281b7eeb135ed40163b987fe8beea4e0d246e7e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-17 09:48:12 +0400

    created endpoints to edit and delete workspace

---
+++ src/features/workspace/api/useEditWorkspace.ts
@@ -0,0 +1,28 @@
+import { ref } from 'vue';
+import { toast } from 'vue-sonner';
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { useConvexMutation } from "@convex-vue/core";
+
+type ResponseType = Id<"workspaces"> | null;
+
+export const useEditWorkspace = () => {
+    const data = ref<ResponseType>(null);
+    const error = ref<Error | null>(null);
+
+    const { isLoading, mutate } = useConvexMutation(
+        api.workspaces.update,
+        {
+            onSuccess() {
+                toast.success("Workspace update");
+            }
+        }
+    );
+
+    return {
+        data,
+        error,
+        mutate,
+        isLoading,
+    };
+};
