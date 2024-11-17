df86e43ba07882426dddeb3410ec526db5ecd353
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-17 12:43:54 +0400

    added some logic to useDeleteWorkspace api hook

--- src/features/workspace/api/useDeleteWorkspace.ts
+++ src/features/workspace/api/useDeleteWorkspace.ts
@@ -4,7 +4,6 @@ import { useRouter } from 'vue-router';
 import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { useConvexMutation } from "@convex-vue/core";
-import { useCollections } from "@/features/workspace/store/useCollections";
 
 type ResponseType = Id<"workspaces"> | null;
 
@@ -13,21 +12,11 @@ export const useDeleteWorkspace = () => {
     const error = ref<Error | null>(null);
     const router = useRouter();
 
-    const { workspaces } = useCollections();
-
     const { isLoading, mutate } = useConvexMutation(
         api.workspaces.remove,
         {
-            onSuccess(deletedId: string) {
-                if (workspaces.value && workspaces.value.length) {
-                    const filtered = workspaces.value.filter(w => w._id !== deletedId);
-
-                    if (filtered.length) {
-                        router.push(`/workspace/${filtered[0]._id}`);
-                    }
-                } else {
-                    router.push('/workspace');
-                }
+            onSuccess() {
+                router.push('/workspace');
                 toast.success("Workspace deleted");
             }
         }
