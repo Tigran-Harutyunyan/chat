fa0ade45f791393d67ec322b2acbe951f444bb91
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-18 12:22:24 +0400

    created workspace view

---
+++ src/views/workspace/WorkspaceView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { watch } from "vue";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+import { useGetWorkspaces } from "@/features/workspace/api/useGetWorkspaces";
+import { useCreateWorkspaceModal } from "@/features/workspace/store/useCreateWorkspaceModal";
+import { useWorkspaceId } from "@/features/workspace/hooks/useWorkspaceId";
+
+const router = useRouter();
+const { onOpen } = useCreateWorkspaceModal();
+const { data, isLoading } = useGetWorkspaces();
+const { workspaceId } = useWorkspaceId();
+watch(
+  () => data.value,
+  () => {
+    if (Array.isArray(data.value)) {
+      if (!data.value.length) {
+        onOpen();
+        return;
+      }
+
+      if (!workspaceId.value) {
+        const id = data.value[0]?._id;
+        router.push(`/workspace/${id}`);
+      }
+    }
+  },
+  {
+    immediate: true,
+  }
+);
+</script>
+
+<template>
+  <DashboardLayout> </DashboardLayout>
+</template>
