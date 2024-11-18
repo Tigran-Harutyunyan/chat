00dee9488610904aa461b3abde8846952eefd493
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-18 12:22:39 +0400

    added new modal

--- src/components/Modals.vue
+++ src/components/Modals.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import CreateWorkspaceModal from "@/features/workspace/components/CreateWorkspaceModal.vue";
+import CreateChannelModal from "@/features/channels/components/CreateChannelModal.vue";
 </script>
 
 <template>
   <CreateWorkspaceModal />
+  <CreateChannelModal />
 </template>
