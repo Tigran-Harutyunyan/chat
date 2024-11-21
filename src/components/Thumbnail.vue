c1a09c1f1b03e1573aa283f4643d81eaf76aae25
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-21 15:21:39 +0400

    created Thumbnail component to host image and show it in dialog

---
+++ src/components/Thumbnail.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { XIcon } from "lucide-vue-next";
+
+interface ThumbnailProps {
+  url: string | null | undefined;
+}
+defineProps<ThumbnailProps>();
+</script>
+
+<template>
+  <Dialog v-if="url">
+    <DialogTrigger>
+      <div
+        class="relative overflow-hidden max-w-[360px] border rounded-lg my-2 cursor-zoom-in"
+      >
+        <img
+          :src="url"
+          alt="Mesage image"
+          class="rounded-md object-cover size-full"
+        />
+      </div>
+    </DialogTrigger>
+    <DialogContent
+      class="max-w-[800px] border-none bg-transparent p-0 shadow-none"
+    >
+      <img
+        :src="url"
+        alt="Mesage image"
+        class="rounded-md object-cover size-full"
+      />
+    </DialogContent>
+  </Dialog>
+</template>
