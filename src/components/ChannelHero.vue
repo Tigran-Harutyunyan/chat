4d9297443d01486e51a64ff09e3162d4c64210dd
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-21 15:22:53 +0400

    created Channel and Conversation Hero components

---
+++ src/components/ChannelHero.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { format } from "date-fns";
+
+interface ChannelHeroProps {
+  name: string;
+  creationTime: number;
+}
+
+defineProps<ChannelHeroProps>();
+</script>
+
+<template>
+  <div class="mt-[88px] mx-5 mb-4">
+    <p class="text-2xl font-bold flex items-center mb-2"># {{ name }}</p>
+    <p class="font-normal text-slate-800 mb-4">
+      This channel was created on {{ format(creationTime, "MMMM do, yyyy") }}.
+      This is the very beginning of the <strong>{{ name }}</strong> channel.
+    </p>
+  </div>
+</template>
---
+++ src/components/ConversationHero.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+interface ConversationHeroProps {
+  name?: string;
+  image?: string;
+}
+
+defineProps<ConversationHeroProps>();
+</script>
+
+<template>
+  <div class="mt-[88px] mx-5 mb-4">
+    <div class="flex items-center gap-x-1 mb-2">
+      <Avatar class="size-14 mr-2">
+        <AvatarImage :src="image" />
+        <AvatarFallback>{{ avatarFallback }}</AvatarFallback>
+      </Avatar>
+      <p class="text-2xl font-bold">
+        {{ name }}
+      </p>
+    </div>
+    <p class="font-normal text-slate-800 mb-4">
+      This conversation is just between you and <strong>{{ name }}</strong>
+    </p>
+  </div>
+</template>
