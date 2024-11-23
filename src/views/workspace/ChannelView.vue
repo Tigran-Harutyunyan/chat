aca81cd9db8ac37046091447a48cadb8a2d509db
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-23 13:32:47 +0400

    created Channel view

---
+++ src/views/workspace/ChannelView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DashboardLayout from "@/layouts/DashboardLayout.vue";
+import ChannelInfo from "@/features/channels/components/ChannelInfo.vue";
+import { useChannelId } from "@/features/channels/hooks/useChannelId";
+import { useGetChannel } from "@/features/channels/api/useGetChannel";
+
+const { channelId } = useChannelId();
+</script>
+
+<template>
+  <DashboardLayout>
+    <ChannelInfo :channel-id="channelId" :key="channelId" />
+  </DashboardLayout>
+</template>
