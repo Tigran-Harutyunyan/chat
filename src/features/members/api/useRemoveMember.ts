4e7b0a279904cc98dfcea2c6b8176765f4f0f74d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-11-24 11:26:29 +0400

    refactored hook

--- src/features/members/api/useRemoveMember.ts
+++ src/features/members/api/useRemoveMember.ts
@@ -1,15 +1,13 @@
 import { api } from "@convex/api";
 import { useConvexMutation } from "@convex-vue/core";
 import { toast } from 'vue-sonner';
-import { useRouter } from "vue-router";
 export const useRemoveMember = () => {
-    const router = useRouter();
+
     const { isLoading, mutate } = useConvexMutation(
         api.members.remove,
         {
             onSuccess() {
-                toast.success("You left the workspace");
-                router.replace("/");
+                toast.success("User left the workspace");
             },
             onError: () => {
                 toast.error("Failed to leave the workspace");
